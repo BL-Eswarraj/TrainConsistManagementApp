f5bb6d27d8778ea6473a18aa0eb53d3cd98bee60
BL-Eswarraj <eswarraj.rajendiren@bridgelabz.com>
2026-06-25 12:00:40 +0530

    Eswar added UC10: Total Seating capacity in bogies

--- src/main/TrainApp.java
+++ src/main/TrainApp.java
@@ -5,7 +5,7 @@ import main.model.Bogie;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistManagementApp {
+public class TrainApp {
 
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
